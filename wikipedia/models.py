cbc7a0014ce6da35ab1415b2475c41e6cba8c5b8
ram <ramusus@gmail.com>
2011-01-28 15:58:19 +0300

    added remove_garbage() and process_content() methods

--- wikipedia/models.py
+++ wikipedia/models.py
@@ -5,7 +5,9 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
+from BeautifulSoup import BeautifulSoup, Comment
 import urllib, urllib2
+import re
 
 LANGUAGES = getattr(settings, 'WIKIPEDIA_LANGUAGES', [('en', _('English'))])
 
@@ -71,6 +73,16 @@ class WikipediaElement(models.Model):
         unique_together = ('object_id', 'content_type', 'lang', 'title')
         ordering = ('-updated',)
 
+    class Remove:
+        edit_links = True
+        contents = True
+        comments = True
+        external_links = True
+        infobox = True
+        sisterproject = True
+        navbox = True
+        disambiguation = True
+
     lang = models.CharField(_('Language'), max_length=2, choices=LANGUAGES)
     title = models.CharField(_('Title'), max_length=300)
     content = models.TextField(_('Content'))
@@ -95,4 +107,66 @@ class WikipediaElement(models.Model):
         '''
         Process wikipedia content before saving to DB
         '''
+        self.content = BeautifulSoup(self.content)
+
+        self.parse_content()
+        self.remove_garbage()
+
+        self.content = unicode(self.content)
+
+    def parse_content(self):
+        '''
+        Parse wikipedia content page for useful information
+        '''
         pass
+
+    def remove_garbage(self):
+        '''
+        Remove unnecessary tags from wikipedia content page
+        '''
+        # span editsection
+        if self.Remove.edit_links:
+            [el.extract() for el in self.content.findAll('span', {'class': 'editsection'})]
+
+        # table contents
+        if self.Remove.contents:
+            [el.extract() for el in self.content.findAll('table', {'id': 'toc', 'class': 'toc'})]
+
+        # remove all comments
+        if self.Remove.comments:
+            [el.extract() for el in self.content.findAll(text=lambda text:isinstance(text, Comment))]
+
+        # external links block (ru,en)
+        if self.Remove.external_links:
+            links_title = self.content.find(text=re.compile(u'(Ссылки|links)'))
+            if links_title:
+                # get h2
+                links_title = links_title.parent.parent
+                links = links_title.findNextSibling('ul')
+                if links:
+                    links_title.extract()
+                    links.extract()
+
+        table_classes = []
+        div_classes = []
+
+        if self.Remove.infobox:
+            table_classes += ['infobox'] # table infobox
+            div_classes += ['infobox'] # div infobox
+
+        if self.Remove.sisterproject:
+            # links to another wikimedia (en) table class="metadata mbox-small plainlinks"
+            table_classes += ['metadata']
+            # links to another wikimedia (ru) div class="infobox sisterproject noprint wikiquote-box"
+            div_classes += ['sisterproject','wikiquote-box']
+
+        if self.Remove.navbox:
+            # links to another movies of director table class="navbox collapsible autocollapse nowraplinks"
+            table_classes += ['navbox','NavFrame']
+
+        if self.Remove.disambiguation:
+            # disambiguation div class="dablink"
+            div_classes += ['dablink']
+
+        [el.extract() for el in self.content.findAll('table', {'class': re.compile('(%s)' % '|'.join(table_classes))})]
+        [el.extract() for el in self.content.findAll('div', {'class': re.compile('(%s)' % '|'.join(div_classes))})]

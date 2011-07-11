123695144ad5c05c32f0b9fe8ab0020d376f2f67
ram <ramusus@gmail.com>
2011-07-11 13:05:38 +0400

    fixed exception text error

--- wikimedia/models.py
+++ wikimedia/models.py
@@ -79,7 +79,7 @@ class WikipageManager(models.Manager):
                 page = self.get_or_create(project=project, lang=lang, title=title)[0]
 
         except urllib2.HTTPError:
-            raise WikipageTitleError('Incorrect wikipedia title (%s)' % (page.get_url()))
+            raise WikipageTitleError('Incorrect %s title (%s) with lang "%s"' % (project_code, title, lang))
 
         # update all found sister projects
         if with_sister_projects:
